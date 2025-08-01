3ab5f8f5a645bc37de25a763d83de653d7a801bb
Robert Safin <robertsafin98@gmail.com>
2025-08-01 22:47:03 +0800

    md

--- examples/fetch/main.go
+++ examples/fetch/main.go
@@ -1,1 +1,47 @@
+//go:build js && wasm
+
 package main
+
+import (
+	"encoding/json"
+
+	"github.com/Robert-Safin/go-wasm/dom"
+	"github.com/Robert-Safin/go-wasm/dom/types/attribute"
+	"github.com/Robert-Safin/go-wasm/dom/types/event"
+	"github.com/Robert-Safin/go-wasm/dom/types/insert"
+	"github.com/Robert-Safin/go-wasm/dom/types/tag"
+	"github.com/Robert-Safin/go-wasm/error"
+	"github.com/Robert-Safin/go-wasm/window/console"
+	"github.com/Robert-Safin/go-wasm/window/fetch"
+)
+
+type Person struct {
+	Name   string `json:"name"`
+	Height string `json:"height"`
+	Mass   string `json:"mass"`
+}
+
+func main() {
+	btn := dom.CreateElement(tag.Button)
+	btn.SetAttribute(attribute.InnerHTML, "fetch")
+	btn.InsertIntoDom(insert.AppendChild)
+
+	btn.AddEventListener(event.Click, func() {
+		fetch.Fetch("GET", "https://swapi.info/api/people", map[string]string{}, "",
+			func(res []byte) {
+				var people []Person
+				json.Unmarshal(res, &people)
+				for _, p := range people {
+					t := dom.CreateElement(tag.P)
+					t.SetAttribute(attribute.InnerHTML, p.Name+" "+p.Height+"cm "+p.Mass+"kg")
+					t.InsertIntoDom(insert.AppendChild)
+				}
+
+			},
+			func(e error.Error) {
+				console.Log(e.Value)
+			})
+	})
+
+	select {}
+}
--- main.go
+++ main.go
@@ -1,3 +1,101 @@
+// required build tag.
+//go:build js && wasm
+
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/Robert-Safin/go-wasm/dom"
+	"github.com/Robert-Safin/go-wasm/dom/types/attribute"
+	"github.com/Robert-Safin/go-wasm/dom/types/event"
+	"github.com/Robert-Safin/go-wasm/dom/types/insert"
+	"github.com/Robert-Safin/go-wasm/dom/types/tag"
+	"github.com/Robert-Safin/go-wasm/error"
+	"github.com/Robert-Safin/go-wasm/signal"
+	"github.com/Robert-Safin/go-wasm/window/console"
+	"github.com/Robert-Safin/go-wasm/window/fetch"
+)
+
+type Person struct {
+	Name   string `json:"name"`
+	Height string `json:"height"`
+	Mass   string `json:"mass"`
+}
+
+func main() {
+	// find container
+	doc := dom.GetDocument()
+	container, ok := dom.GetElementById(doc, "container")
+
+	// or create if it doesnt exist
+	if !ok {
+		container = dom.CreateElement(tag.Div)
+		container.SetAttribute(attribute.ID, "container")
+		dom.InsertIntoDom(container, insert.After)
+	}
+
+	// set some styles nito style attribute
+	container.SetStyles(map[string]string{
+		"background-color": "grey",
+		"width":            "50%",
+		"height":           "100%",
+	})
+	// ship with .css files or fetch tailwind stylesheet
+	container.AddClasses("flex", "flex-row")
+
+	// Siagnals as reactivity system, stores generic type.
+	// Requires initial value and equality funciton.
+	// Equality func used to prevent re-renders when new state == prev state.
+	// Equality func can be nil, in which case all updates trigger re-renders.
+	// Return clean up function, that empties underlying map of effect on a signal.
+	count, killSig := signal.NewSignal[int](0, func(a int, b int) bool { return a == b })
+	defer killSig()
+
+	// element to render signal value
+	p := dom.CreateElement(tag.P)
+	p.SetAttribute(attribute.InnerHTML, strconv.Itoa(count.Get()))
+	container.AppendChild(p)
+
+	// this element will store value of some decoded JSON.
+	p2 := dom.CreateElement(tag.P)
+	container.AppendChild(p2)
+
+	// Add an effect to the signal. Effect is a function that will run if
+	// the value of signal changes (count.Set() in this case) unless prevented by Equality func.
+	// Any number of effects can be registered to a signal.
+	// .Effect returns cleanup function, that will delete only that effect from a signal's map.
+	unsub := count.Effect(func() {
+		// update 'counter' on signal change
+		p.SetAttribute(attribute.InnerHTML, strconv.Itoa(count.Get()))
+		// fire a http request and decode it into HTML content
+		fetch.Fetch("GET", "https://swapi.info/api/people/"+strconv.Itoa(count.Get()), map[string]string{}, "",
+			// promise resolved
+			func(bytes []byte) {
+				var person Person
+				json.Unmarshal(bytes, &person)
+				p2.SetAttribute(attribute.InnerHTML, person.Name)
+
+			},
+			// promise resolved rejected
+			func(e error.Error) {
+				console.Log(e.Value)
+			})
+	})
+	defer unsub()
+
+	// add button that will trigger signal updatess
+	btn := dom.CreateElement(tag.Button)
+	btn.SetAttribute(attribute.InnerHTML, "click me")
+	container.PrependChild(btn)
+
+	// attach event listener.
+	// returns a clean-up function to remove listener from DOM.
+	cleanup := btn.AddEventListener(event.Click, func() {
+		count.Set(count.Get() + 1)
+	})
+	defer cleanup()
+	// keep go runtime alive
+	select {}
+}
